9d191e2922a7b82fcd4ec4c101af78e76f458d54
Keturah Howard <keturah.dev@gmail.com>
2020-02-14 11:28:22 -0800

    change specs test-symptom and merge first 2 specs/accomplish 1st spec

--- src/main.js
+++ src/main.js
@@ -4,11 +4,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+function getDoctors(doctors) {
+  console.log(doctors[0].profile.first_name)
+  let output = $('#doctor-list')
+  output.append(`<li>${doctors[0].profile.first_name} ${doctors[0].profile.last_name}</li>`)
+}
+
+async function order(input) {
+  let doctors = new Doctors(input);
+  await doctors.getFetch()
+  await getDoctors(doctors.jsonifiedDoctors);
+}
+
 $(document).ready(function () {
   $('form#doctor').submit(function (event) {
     event.preventDefault();
     let input = $('#issue').val();
-    let doctors = new Doctors(input);
-    doctors.getFetch()
+    order(input);
   })
 })
