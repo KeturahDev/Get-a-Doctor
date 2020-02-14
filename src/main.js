c51f034d5c7f4e9916a54592c13fd7b96a24b3b6
Keturah Howard <keturah.dev@gmail.com>
2020-02-14 12:10:43 -0800

    add if statements to achieve second spec

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 function getDoctors(doctors) {
-  console.log(doctors[0].profile.first_name)
-  let output = $('#doctor-list')
-  output.append(`<li>${doctors[0].profile.first_name} ${doctors[0].profile.last_name}</li>`)
+  // console.log(doctors[0].profile.first_name)
+  if (doctors === false) {
+    $('.error').html('Looks like theres no doctors in the area that meet that criteria. Try entering a one-word symptom, or name.');
+  } else {
+    // doctors.forEach(doctor => {
+    //   let output = $('#doctor-list')
+    //   output.append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`)
+    // });
+    let output = $('#doctor-list')
+    output.append(`<li>${doctors[0].profile.first_name} ${doctors[0].profile.last_name}</li>`)
+  }
 }
 
 async function order(input) {
@@ -21,5 +29,6 @@ $(document).ready(function () {
     event.preventDefault();
     let input = $('#issue').val();
     order(input);
+    $('.error').empty()
   })
 })
