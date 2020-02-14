47d3501e09e755a2dccdb53534fe07ad5e9d29b9
Keturah Howard <keturah.dev@gmail.com>
2020-02-14 13:12:50 -0800

    add functionality for user to input and search by name- 5th spec

--- src/doctor.js
+++ src/doctor.js
@@ -1,16 +1,17 @@
 export class Doctors {
-  constructor(input) {
-    this.input = input;
+  constructor(issue, name) {
+    this.issue = issue;
+    this.name = name;
     this.jsonifiedDoctors = '';
   }
 
   async getFetch() {
     try {
-      let fetchedResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${this.input}&location=45.523064%2C%20-122.676483%2C100&user_location=45.523064%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
-      let jsonResponse = await fetchedResponse.json()
+      let fetchedResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.issue}&location=45.523064%2C%20-122.676483%2C100&user_location=45.523064%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonResponse = await fetchedResponse.json();
       this.jsonifiedDoctors = jsonResponse.data;
       if (this.jsonifiedDoctors.length < 1) {
-        this.jsonifiedDoctors = false
+        this.jsonifiedDoctors = false;
       }
       console.log('response is:', jsonResponse);
       console.log('specified response is:', this.jsonifiedDoctors);
